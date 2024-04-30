6cf348748aafdc88d43a0c373395482b7ae8ba2b
Giovanni Scivoli <scivoligiovanni@gmail.com>
2024-04-30 18:00:07 +0200

    add

---
+++ src/components/todoCardList/TodoCardList.jsx
@@ -0,0 +1,23 @@
+import TodoCard from "../todoCard/TodoCard";
+import styles from "./index.module.scss";
+import { useState, useEffect } from "react";
+
+const TodoCardList = () => {
+  const [todoData, setTodoData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/todos")
+      .then((res) => res.json())
+      .then((data) => setTodoData(data.data));
+  }, []);
+
+  return (
+    <div>
+      {todoData.map((todo, index) => {
+        <TodoCard key={index} todoData={todo} />;
+      })}
+    </div>
+  );
+};
+
+export default TodoCardList;
